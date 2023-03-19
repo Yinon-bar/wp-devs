c64094c4214c7c4df829dae61d67d9b491647163
Yinon Bar <63056754+Yinon-bar@users.noreply.github.com>
2023-03-19 09:34:19 +0200

    Update footer.php

--- themes/wp-devs/footer.php
+++ themes/wp-devs/footer.php
@@ -5,7 +5,7 @@
     </div>
     <nav class="footer-menu">
       <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)); ?>
-      <?php echo "end test" ?>
+      <?php echo "end test2" ?>
     </nav>
   </div>
 </footer>
